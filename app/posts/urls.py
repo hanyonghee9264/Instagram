022c85cb9b21028e52887b41142311e6e3a3a02f
hanyonghee <hanyonghee9264@gmail.com>
2018-10-19 16:22:13 +0900

    posts.views.post_create, posts/post_create.html연결 및 템플릿 form구현

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -7,4 +7,5 @@ app_name = 'posts'
 urlpatterns = [
     # posts.urls내의 패턴들은, perfix가 '/posts/'임
     path('', views.post_list, name='post-list'),
+    path('create/', views.post_create, name='post-create'),
 ]
