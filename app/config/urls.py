aafdd36740995b806dfa046f49212e717e6148e2
hanyonghee <hanyonghee9264@gmail.com>
2018-11-22 11:25:41 +0900

    posts urls 수정

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,5 +1,4 @@
 """config URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
@@ -18,29 +17,27 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from posts import apis
 
-from posts.views import tag_post_list
 from . import views
+from posts.views import tag_post_list
 from posts import apis as posts_apis
 from members import apis as members_apis
 
 urlpatterns_api_posts = ([
-    path('post/', apis.PostList.as_view(), name='post-list'),
-    path('post/<int:pk>/', apis.PostDetail.as_view(), name='post-detail'),
-    path('post/<int:post_pk>/like/', apis.PostLikeCreateDestroy.as_view(), name='post-like'),
+    path('post/', posts_apis.PostList.as_view(), name='post-list'),
+    path('post/<int:pk>/', posts_apis.PostDetail.as_view(), name='post-detail'),
+    path('post/<int:post_pk>/like/', posts_apis.PostLikeCreateDestroy.as_view(), name='post-like'),
 
-    path('postlike/', apis.PostLikeCreateAPIView.as_view()),
-    path('postlike/<int:pk>/', apis.PostLikeDestroyAPIView.as_view()),
+    path('postlike/', posts_apis.PostLikeCreateAPIView.as_view()),
+    path('postlike/<int:pk>/', posts_apis.PostLikeDestroyAPIView.as_view()),
 ], 'posts')
 
 urlpatterns_api_members = ([
     path('auth-token/', members_apis.AuthTokenView.as_view()),
     path('user/profile/', members_apis.UserDetail.as_view()),
     path('user/<int:pk>/', members_apis.UserDetail.as_view()),
-
-    path('user/view/<int:pk>/', members_apis.UserDetailAPIView.as_view()),
     path('user/view/profile/', members_apis.UserDetailAPIView.as_view()),
+    path('user/view/<int:pk>/', members_apis.UserDetailAPIView.as_view()),
 ], 'members')
 
 urlpatterns_api = ([
@@ -54,11 +51,12 @@ urlpatterns = [
     path('', RedirectView.as_view(pattern_name='posts:post-list'), name='index'),
     # /posts/로 들어오는 URL은 posts.urls모듈에서 처리
     path('posts/', include('posts.urls')),
-    path('explore/tags/<str:tag_name>/', tag_post_list, name='tag-post-list'),
+    path('explore/tags/<str:tag_name>/',
+         tag_post_list,
+         name='tag-post-list'),
     path('members/', include('members.urls')),
 
     path('api/', include(urlpatterns_api)),
-
 ]
 # MEDIA_URL로 시작하는 URL은 static()내의 serve() 함수를 통해 처리
 # MEDIA_ROOT기준으로 파일을 검색함
